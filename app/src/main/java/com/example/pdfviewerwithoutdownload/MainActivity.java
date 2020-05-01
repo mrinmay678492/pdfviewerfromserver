83eb69cc4f253c962480b125af92dc32c8f1d315
mrinmay678492 <mrinmay492@gmail.com>
2020-05-02 01:05:28 +0530

    PDFVIEWER

---
+++ app/src/main/java/com/example/pdfviewerwithoutdownload/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.pdfviewerwithoutdownload;
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    public static List<PDFModel> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.RV);
+
+        list = new ArrayList<>();
+        list.add(new PDFModel("",""));
+        list.add(new PDFModel("", ""));
+        list.add(new PDFModel("",""));
+        list.add(new PDFModel("",""));
+        list.add(new PDFModel("",""));
+        list.add(new PDFModel("",""));
+        list.add(new PDFModel("",""));
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+        ItemClickListener itemClickListener = new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position, boolean isLongClick) {
+                Intent intent = new Intent(MainActivity.this,PDFActivity.class);
+                //intent.putExtra("url",list.get(position).getPdfUrl());
+                intent.putExtra("position",position);
+                startActivity(intent);
+            }
+        };
+
+        PDFAdapter adapter = new PDFAdapter(list,this,itemClickListener);
+        recyclerView.setAdapter(adapter);
+    }
+}
